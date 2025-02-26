9039e890a8f56c5ae308b3b71d0da1017411a07d
Priyanka Panga <priyankapanga17@gmail.com>
2025-02-26 07:56:08 -0500

    Add files via upload

---
+++ DiaryPal_NewApp.swift
@@ -0,0 +1,19 @@
+//
+//  DiaryPal_NewApp.swift
+//  DiaryPal_New
+//
+//  Created by Priyanka on 08/02/25.
+//
+
+import SwiftUI
+
+@main
+
+struct DiaryPal_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DiaryView()
+        }
+    }
+}
+
